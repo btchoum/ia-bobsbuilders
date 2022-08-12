4ecc4c4bd9ae94f78889fe4bc5e5dc61fc28ca2a
btchoum <bertrand.tchoumkeu@gmail.com>
2022-08-12 10:48:25 -0500

    Ignore Because we're not working on the Map class yet

--- LevelUpGame.Tests/MapTest.cs
+++ LevelUpGame.Tests/MapTest.cs
@@ -5,16 +5,17 @@ using LevelUpGame.levelup;
 namespace DotNetExample.Tests.Steps;
 
 
-    public class MapTest
+public class MapTest
+{
+    [Ignore("Because we're not working on the Map class yet")]
+    [Test]
+    public void calculatePosition()
     {
-        [Test]
-        public void calculatePosition()
-        {
-            Map map = new Map();
-            Position pos = new Position();
-            Position newPos = map.CalculatePosition(pos, DIRECTION.EAST);
-            Assert.AreEqual(2, newPos.X);
-        }
-
+        Map map = new Map();
+        Position pos = new Position();
+        Position newPos = map.CalculatePosition(pos, DIRECTION.EAST);
+        Assert.AreEqual(2, newPos.X);
     }
+
+}
 
